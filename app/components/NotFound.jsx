7595b775e35e17f8c2b933a9c5de5209ebbba82b
Hannntastix <162966950+Hannntastix@users.noreply.github.com>
2024-11-27 00:48:05 +0700

    Minor Issue again

--- app/components/NotFound.jsx
+++ app/components/NotFound.jsx
@@ -6,7 +6,7 @@ export default function NotFound() {
     return (
         <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
             <h1 className="text-6xl font-bold text-gray-800 mb-4">404</h1>
-            <h1 className='text-center font-bold text-3xl'>Please Log In First! or refresh the website</h1>
+            <h1 className="text-center font-bold text-3xl">Please Log In First! or refresh the website</h1>
             <p className="text-xl text-gray-600 text-center">
                 Oops! The page you are looking for doesn't exist.
             </p>
